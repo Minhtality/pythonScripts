9c1311bca75c5a65d687a3e2afdda1ce6708d14e
Minh Tran <mtran1712@gmail.com>
2024-04-29 00:02:51 -0700

    updated request to one

--- discord_bot.py
+++ discord_bot.py
@@ -19,8 +19,8 @@ def send_reminder_check():
         webhook.send(content=message)
 
 
-def get_usd_to_yen():
-    url = "https://api.apilayer.com/exchangerates_data/latest?symbols=JPY&base=USD"
+def get_usd_exchange_rate():
+    url = "https://api.apilayer.com/exchangerates_data/latest?symbols=JPY%2CKRW&base=USD"
     payload = {}
     headers = {
         "apikey": forex_api_key
@@ -28,7 +28,7 @@ def get_usd_to_yen():
     response = requests.request("GET", url, headers=headers, data=payload)
     if response.status_code == 200:
         data = response.json()
-        return data['rates']['JPY']
+        return data['rates']['JPY'], data['rates']['KRW']
     else:
         return None
 
@@ -37,9 +37,11 @@ if __name__ == "__main__":
     send_reminder_check()
 
     # Call the function to get the yen to USD exchange rate
-    yen_to_usd_rate = get_usd_to_yen()
-
-    if yen_to_usd_rate is not None:
-        todayFormat = today.strftime("%m/%d/%Y")
-        webhook.send(
-            content=f"@here Today ({todayFormat}) USD to YEN exchange rate: {yen_to_usd_rate}")
+    exchange_rate = get_usd_exchange_rate()
+    if exchange_rate:
+        jpy, krw = exchange_rate
+        message = f"@here: \n JPY to USD: {round(jpy,2)}\n KRW to USD: {round(krw,2)}"
+        webhook.send(content=message)
+    else:
+        message = "Failed to get the exchange rate."
+        webhook.send(content=message)
