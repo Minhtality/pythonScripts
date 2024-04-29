117e1ee9d56124a172ab9d2ad3e0ba097cbfc624
Minh Tran <mtran1712@gmail.com>
2024-04-28 22:43:11 -0700

    rent reminder lmao

--- discord_bot.py
+++ discord_bot.py
@@ -1,9 +1,16 @@
 from discord import SyncWebhook
 from dotenv import load_dotenv
 import os
+import datetime
 
 load_dotenv()
 
 discord_url = os.getenv('DISCORD_URL')
 webhook = SyncWebhook.from_url(discord_url)
-webhook.send("@everyone This is a test message.")
+
+# if date is first day of the month
+
+today = datetime.date.today()
+if today.day == 1:
+    message = "@here Your rent is due."
+    webhook.send(message)
