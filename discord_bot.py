5a539b176b28b9c08e9788250aa6f23b06980c02
Minh Tran <mtran1712@gmail.com>
2024-04-28 22:34:49 -0700

    feat: updated env variable and test message

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,9 @@
 from discord import SyncWebhook
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 discord_url = os.getenv('DISCORD_URL')
 webhook = SyncWebhook.from_url(discord_url)
-webhook.send("Hello World")
+webhook.send("@everyone This is a test message.")
